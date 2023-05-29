cd7869276440ac5f459d7f5bb31d8c2795490d06
diegoG82 <diegogiardina@hotmail.it>
2023-05-29 16:22:04 +0200

    created index with comics printed

---
+++ resources/views/comics/index.blade.php
@@ -0,0 +1,40 @@
+
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <h2>TUTTI I COMICS</h2>
+        <div class="my-2 text-end">
+            {{-- <a href="{{ route('comics.create') }}">Crea una nuovo fumetto</a> --}}
+        </div>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">id</th>
+                    <th scope="col">Titolo</th>
+                    <th scope="col">Descrizione</th>
+                    <th scope="col">Prezzo</th>
+                    <th scope="col">Serie</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($comics as $comic)
+                    <tr>
+                        <th scope="row">{{ $comic->id }}</th>
+                        <td>{{ $comic->title }}</td>
+                        <td>{{ $comic->description }}</td>
+                        <td>{{ $comic->series }}</td>
+                        <td>
+                            {{-- <a class="btn btn-success" href="{{ route('comic.show', $comic->id) }}"> --}}
+                                <i class="fa-solid fa-eye"></i>
+                            </a>
+                        </td>
+                    </tr>
+                @endforeach
+
+            </tbody>
+        </table>
+    </div>
+@endsection
+
+
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\ComicController;
 use App\Http\Controllers\Admin\HomeController;
 use Illuminate\Support\Facades\Route;
 
@@ -13,7 +14,9 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', [ComicController::class, 'index'])->name('home');
 
-Route::get('/', [HomeController::class, 'index'])->name('home');
+
+// Route::get('/', [HomeController::class, 'index'])->name('home');
     
-
+// Route::resource('comics', ComicController::class);
