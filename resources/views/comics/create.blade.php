d55862e5a5cf6a87c0edfad92fc15731243bcf74
diegoG82 <diegogiardina@hotmail.it>
2023-05-30 16:26:17 +0200

    fixed create page

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -7,34 +7,38 @@
         <form action="{{ route('comics.store') }}" method="POST">
             @csrf
             <div class="mb-3">
-                <label for="title" class="form-label">Titolo</label>
+                <label for="title" class="form-label">Title</label>
                 <input type="text" class="form-control" id="title" name="title">
             </div>
 
             <div class="mb-3">
-                <label for="description" class="form-label">Descrizione</label>
+                <label for="description" class="form-label">Description</label>
                 <textarea class="form-control" name="description" id="description" rows="3"></textarea>
             </div>
 
             <div class="mb-3">
-                <label for="thumb" class="form-label">Immagine</label>
+                <label for="thumb" class="form-label">Thumb</label>
                 <input type="text" class="form-control" id="thumb" name="thumb">
             </div>
             
 
             <div class="mb-3">
-                <label for="price" class="form-label">Prezzo</label>
+                <label for="price" class="form-label">Price</label>
                 <input type="text" class="form-control" id="price" name="price">
             </div>
 
             <div class="mb-3">
-                <label for="type" class="form-label">Tipo</label>
-                <input type="text" class="form-control" id="type" name="type">
+                <label for="type" class="form-label">Type</label>
+                <select class="form-select" id="type" name="type">
+                    <option value="Comic">Comic</option>
+                    <option value="Graphic Novel">Graphic Novel</option>
+                </select>
             </div>
+            
             
 
             <div class="mb-3">
-                <label for="series" class="form-label">Serie</label>
+                <label for="series" class="form-label">Series</label>
                 <input type="text" class="form-control" id="series" name="series">
             </div>
 
